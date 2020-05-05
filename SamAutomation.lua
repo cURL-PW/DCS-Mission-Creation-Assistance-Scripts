b91e6db4918180111392187a7682f468f9ae85d9
cURL-PW <ryougisiki@gmail.com>
2020-05-05 21:18:01 +0900

    Update SamAutomation.lua

--- SamAutomation.lua
+++ SamAutomation.lua
@@ -32,51 +32,51 @@ function SEAD_launch:onEvent(event)
             --MinStartDelay 停波した後、送波するまでの最小時間
             --MaxStartDelay 停波した後、送波するまでの最大時間
             if _targetMimTypeName == "1L13 EWR" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "55G6 EWR" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "p-19 s-125 sr" then
-                Suppressed(id,100,15,20,40,80)
+                SuppressedUnit(id,100,15,20,40,80)
             elseif _targetMimTypeName == "Dog Ear radar" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "SNR_75V" then
-                Suppressed(id,100,10,20,40,80)
+                SuppressedUnit(id,100,10,20,40,80)
             elseif _targetMimTypeName == "snr s-125 tr" then
-                Suppressed(id,100,10,15,20,40)
+                SuppressedUnit(id,100,10,15,20,40)
             elseif _targetMimTypeName == "Kub 1S91 str" then
-                Suppressed(id,100,15,20,40,60)
+                SuppressedGroup(id,100,15,20,40,60) --SA6のみ対象を停波させても２バンドあるせいか止まらないのでグループで止める
             elseif _targetMimTypeName == "Osa 9A33 ln" then
-                Suppressed(id,100,7,10,30,50)
+                SuppressedUnit(id,100,7,10,30,50)
             elseif _targetMimTypeName == "S-300PS 40B6M tr" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "S-300PS 40B6MD sr" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "S-300PS 64H6E sr" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "SA-11 Buk SR 9S18M1" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "SA-11 Buk LN 9A310M1" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "Tor 9A331" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "Hawk tr" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "Hawk sr" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "Patriot str" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "Hawk cwar" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             elseif _targetMimTypeName == "Roland Radar" then
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             else
-                Suppressed(id,100,10,11,10,11)
+                SuppressedUnit(id,100,10,11,10,11)
             end
         end
     end
 end
 
-function Suppressed(id,Suppresse_rate,MinOffDelay,MaxOffDelay,MinStartDelay,MaxStartDelay)
+function SuppressedUnit(id,Suppresse_rate,MinOffDelay,MaxOffDelay,MinStartDelay,MaxStartDelay)
     local _evade = mist.random (1,100) -- random number for chance of evading action
     if (_evade <= Suppresse_rate) then --ここで何割起動するか設定する
         local RadarOffDelay = math.random(MinOffDelay, MaxOffDelay) --ミサイル発射時から何秒後に停波するか決める
@@ -96,6 +96,26 @@ function Suppressed(id,Suppresse_rate,MinOffDelay,MaxOffDelay,MinStartDelay,MaxS
     end
 end
 
+function SuppressedGroup(id,Suppresse_rate,MinOffDelay,MaxOffDelay,MinStartDelay,MaxStartDelay)
+    local _evade = mist.random (1,100) -- random number for chance of evading action
+    if (_evade <= Suppresse_rate) then --ここで何割起動するか設定する
+        local RadarOffDelay = math.random(MinOffDelay, MaxOffDelay) --ミサイル発射時から何秒後に停波するか決める
+        local RadarOnDelay = math.random(MinStartDelay, MaxStartDelay) --停波後から何秒後に起動するか決める
+        if SuppressedGroups[id.groupName] == nil then
+            SuppressedGroups[id.groupName] = {
+                SuppressionStartTime = timer.getTime() + RadarOffDelay,
+                SuppressionEndTime = timer.getTime() + RadarOnDelay +  RadarOffDelay,
+                Missileobj = _SEADmissileName,
+                SuppressionEndN = SuppressionEndCounter	--Store instance of SuppressionEnd() scheduled function
+            }
+            timer.scheduleFunction(SuppressionGroupStart, id, SuppressedGroups[id.groupName].SuppressionStartTime)	--Schedule the SuppressionStart() function
+            timer.scheduleFunction(SuppressionGroupEnd, id, SuppressedGroups[id.groupName].SuppressionEndTime)	--Schedule the SuppressionEnd() function
+            if debugmode == true then trigger.action.outText( string.format("Radar Off " ..string.format(RadarOffDelay)), 20) end --debug message
+            if debugmode == true then trigger.action.outText( string.format("Radar On " ..string.format(RadarOnDelay)), 20) end --debug message
+        end
+    end
+end
+
 function SuppressionEnd(id)
     id.ctrl:setOption(AI.Option.Ground.id.ALARM_STATE,AI.Option.Ground.val.ALARM_STATE.RED)
     SuppressedGroups[id.groupName] = nil
@@ -104,6 +124,16 @@ end
 function SuppressionStart(id)
     id.ctrl:setOption(AI.Option.Ground.id.ALARM_STATE,AI.Option.Ground.val.ALARM_STATE.GREEN)
 end
+
+function SuppressionGroupEnd(id)
+    id.groupName:getController():setOption(AI.Option.Ground.id.ALARM_STATE,AI.Option.Ground.val.ALARM_STATE.RED)
+    SuppressedGroups[id.groupName] = nil
+end
+
+function SuppressionGroupStart(id)
+    id.groupName:getController():setOption(AI.Option.Ground.id.ALARM_STATE,AI.Option.Ground.val.ALARM_STATE.GREEN)
+end
+
 
 function table.in_value (tbl, val)
     for k, v in pairs (tbl) do
